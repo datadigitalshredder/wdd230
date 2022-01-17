d2b5a1588083c61ad323782d46fe534f936a1e29
datadigitalshredder <hov21003@byui.edu>
2022-01-17 13:46:27 +0200

    Week 1 corrections

--- js/portal.js
+++ js/portal.js
@@ -1,2 +1,4 @@
+const year = document.getElementById("year");
+year.innerHTML = new Date().getFullYear();
 const lastmod = document.querySelector('#lastmod');
 lastmod.innerHTML = `<strong>Last modified</strong>: ${document.lastModified}`;
