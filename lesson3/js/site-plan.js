34cd3185bf2b5375382172b63900c722c6e4daac
datadigitalshredder <hov21003@byui.edu>
2022-01-23 02:04:01 +0200

    Week 3 lesson

---
+++ lesson3/js/site-plan.js
@@ -0,0 +1,4 @@
+const year = document.getElementById("year");
+year.innerHTML = new Date().getFullYear();
+const lastmod = document.querySelector('#lastmod');
+lastmod.innerHTML = `<strong>Last updated</strong>: ${document.lastModified}`;
