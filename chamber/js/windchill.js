6e0c51ee2d51d3f44b1be9262212273192c57950
datadigitalshredder <hov21003@byui.edu>
2022-02-20 06:14:39 +0200

    week7 almost

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -15,6 +15,7 @@ document.querySelector('#windchill').innerHTML = windChill;
 
 function computeWindChill(temp, speed) {
     windChillFactor = 35.74 + (0.6216 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+    //windChillFactorCelcius = 13.12 + 0.6215T – 11.37 (V^0.16) + 0.3965T (V^0.16)
     windChillFactorRounded = Math.round(windChillFactor * 10) / 10;
     return windChillFactorRounded;
 }
