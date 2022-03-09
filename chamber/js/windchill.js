3799e84222187f2595b5d5a634ec2f5a92d74255
datadigitalshredder <hov21003@byui.edu>
2022-03-09 16:31:21 +0200

    Week 10.5

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,9 +1,9 @@
 // WNDCHILL CALCULATION
-let t = document.querySelector('#weather-temp').textContent;
+let t = document.querySelector('#current-temp').textContent;
 let s = document.querySelector('#windspeed').textContent;
 let windChill = '';
 
-if (t <= 50 && s > 3) {
+if (t <= 10 && s > 4.828032) {
     windChill = computeWindChill(t,s);
     windChill = `${windChill} &#176;F`;
 } 
@@ -14,8 +14,60 @@ else {
 document.querySelector('#windchill').innerHTML = windChill;
 
 function computeWindChill(temp, speed) {
-    windChillFactor = 35.74 + (0.6216 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
-    //windChillFactorCelcius = 13.12 + 0.6215T – 11.37 (V^0.16) + 0.3965T (V^0.16)
-    windChillFactorRounded = Math.round(windChillFactor * 10) / 10;
+    //windChillFactor = 35.74 + (0.6216 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+    windChillFactorCelcius = 13.12 + (0.6215 * temp) - (11.37 * Math.pow(speed, 0.16)) + (0.3965 * temp * Math.pow(speed, 0.16));
+    windChillFactorRounded = Math.round(windChillFactorCelcius * 10) / 10;
     return windChillFactorRounded;
 }
+
+// USING THE WEATHER API
+let unix_timestamp = 1549312452
+// Create a new JavaScript Date object based on the timestamp
+// multiplied by 1000 so that the argument is in milliseconds, not seconds.
+var date = new Date(unix_timestamp * 1000);
+// Hours part from the timestamp
+var hours = date.getHours();
+// Minutes part from the timestamp
+var minutes = "0" + date.getMinutes();
+// Seconds part from the timestamp
+var seconds = "0" + date.getSeconds();
+
+// Will display time in 10:30:23 format
+var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=878549&q=Zvishavane&units=metric&appid=164e183ac818600411c3484dc71c4f9f";
+
+fetch(apiURL)
+    .then((response) => response.json())
+
+    .then((jsObject) => {
+        console.log(jsObject);
+        document.querySelector('#current-temp').textContent = Math.round((jsObject.main.temp) * 10) / 10; // Carefully follow the path to the temp. Note there are different temps for different parts to the city
+        const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+        const desc = jsObject.weather[0].description;
+        const windSpeed = jsObject.wind.speed * 3.6;
+        //const weatherTimeStamp = jsObject.dt;
+        // Data receiving time update:
+        let unix_timestamp = jsObject.dt
+        // Create a new JavaScript Date object based on the timestamp
+        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        const date = new Date(unix_timestamp * 1000);
+        // Hours part from the timestamp
+        const hours = date.getHours();
+        // Minutes part from the timestamp
+        const minutes = "0" + date.getMinutes();
+        // Seconds part from the timestamp
+        const seconds = "0" + date.getSeconds();
+        // Will display time in 10:30:23 format
+        const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        
+        const weatherTimeStamp = formattedTime;
+
+        //document.querySelector('#icon-src').textContent = iconsrc;
+        document.querySelector('#weathericon').setAttribute('src', iconsrc);
+        document.querySelector('#weathericon').setAttribute('alt', desc);
+        document.querySelector('figcaption').textContent = desc;
+        document.querySelector('#windspeed').textContent = Math.round(windSpeed * 10) /10;
+        document.querySelector('#weather-timestamp').textContent = weatherTimeStamp;
+
+    });
--- lesson10/weatherjs/weatherapi.js
+++ lesson10/weatherjs/weatherapi.js
@@ -1,13 +1,13 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5861897&q=Fairbanks&units=imperial&appid=164e183ac818600411c3484dc71c4f9f";
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5861897&q=Fairbanks&units=imperial&appid=164e183ac818600411c3484dc71c4f9f";
 
 fetch(apiURL)
     .then((response) => response.json())
 
     .then((jsObject) => {
         console.log(jsObject);
-        document.querySelector('#current-temp').textContent = Math.round((jsObject.list[0].main.temp) * 10) / 10; // Carefully follow the path to the temp. Note there are different temps for different parts to the city
-        const iconsrc= `https://openweathermap.org/img/w/${jsObject.list[0].weather[0].icon}.png`;
-        const desc = jsObject.list[0].weather[0].description;
+        document.querySelector('#current-temp').textContent = Math.round((jsObject.main.temp) * 10) / 10; // Carefully follow the path to the temp. Note there are different temps for different parts to the city
+        const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+        const desc = jsObject.weather[0].description;
         document.querySelector('#icon-src').textContent = iconsrc;
         document.querySelector('#weathericon').setAttribute('src', iconsrc);
         document.querySelector('#weathericon').setAttribute('alt', desc);
