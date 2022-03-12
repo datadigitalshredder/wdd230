50fce611186d5e63387b39a88d47991808a9cc45
datadigitalshredder <hov21003@byui.edu>
2022-03-12 08:20:02 +0200

    week 9.5

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -124,4 +124,53 @@ if (numVisits > 0) {
 }
 
 // JSON (Directory page)
-const requestUrl = "https://datadigitalshredder.github.io/wdd230/chamber/directory.html"
+const requestUrl = "https://datadigitalshredder.github.io/wdd230/chamber/data.json";
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+
+    const companies = jsonObject['companies'];
+    
+    companies.forEach(displayCompanies); 
+  });
+
+function displayCompanies(company) { // Create elements to add to the document
+    let card = document.createElement('section'); 
+    let img = document.createElement('img');
+    let h2 = document.createElement('h2');
+    let tradeHours = document.createElement('p');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let site = document.createElement('p');
+            
+    // Change the textContent property of the h2 element to contain the company's details
+    img.setAttribute('src', company.logo);
+    img.setAttribute('alt', `Logo of ${company.name}`);
+    h2.textContent = `${company.name}`;
+    tradeHours.textContent = `${company.open}`;
+    address.textContent = `${company.address}`;
+    phone.textContent = `${company.phone}`;
+    site.textContent = `${company.website}`;
+
+    //birthDate.textContent = `Birth Date: ${prophet.birthdate}`;
+    //birthPlace.textContent =`Birth Place: ${prophet.birthplace}`;
+    
+    
+    // Add/append the section(card) with the h2 element
+    card.appendChild(img);
+    card.appendChild(h2);
+    card.appendChild(tradeHours);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(site);
+    //card.appendChild(birthDate);
+    //card.appendChild(birthPlace);
+    
+
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+  }
