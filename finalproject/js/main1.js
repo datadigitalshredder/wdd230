19372836598e7e22d3f538934d3208247af764aa
datadigitalshredder <hov21003@byui.edu>
2022-04-01 16:31:03 +0200

    Week 13 Work 7

--- finalproject/js/main1.js
+++ finalproject/js/main1.js
@@ -307,3 +307,109 @@ fetch(templeUrl)
             };
     });
 
+    // WORKING WITH SUITES DATA
+const suitesURL = "https://datadigitalshredder.github.io/wdd230/finalproject/jsonfolder/services.json";
+fetch(suitesURL)
+    .then((response3) => response3.json())
+
+    .then((jsObject3) => {
+            // Randomly select a temple for the home banner
+            // let temple =  jsObject3.temples;
+
+            // for (let i = 0; i < temple.length; i++){
+
+            //     randomTemple = temple[Math.floor(Math.random() * temple.length)];
+            //     const iconsrc = `${randomTemple.photo}`;
+            //     const desc = `Photo of ${randomTemple.name}`;
+            //     document.querySelector('#home-image').setAttribute('src', iconsrc);
+            //     document.querySelector('#home-image').setAttribute('alt', desc);
+            // };
+
+            //Randomly select suites and services to share details
+            // Suite/ service 1
+            let suitesDetails = jsObject3.suites;
+            // let randomSuitesDetails = suitesDetails[Math.floor(Math.random() * suitesDetails.length)]; // Place this random selection variable outisde the for loop, unlike in the loop above, otherwise the randomly selected city weather might not match the city you want.
+            for (let i = 0; i < suitesDetails.length; i++){
+                let randomSuitesDetails1 = suitesDetails[Math.floor(Math.random() * suitesDetails.length)]; // Place this random selection variable outisde the for loop, unlike in the loop above, otherwise the randomly selected city weather might not match the city you want.
+
+                
+                const iconsrcSuites1 = `${randomSuitesDetails1.photo}`;
+                const descSuites1 = `Photo of ${randomSuitesDetails1.name}`;
+            // Suite/ service 2
+            for (let i = 0; i < suitesDetails.length; i++){
+                let randomSuitesDetails2 = suitesDetails[Math.floor(Math.random() * suitesDetails.length)]; // Place this random selection variable outisde the for loop, unlike in the loop above, otherwise the randomly selected city weather might not match the city you want.
+
+                
+                const iconsrcSuites2 = `${randomSuitesDetails2.photo}`;
+                const descSuites2 = `Photo of ${randomSuitesDetails2.name}`;
+            // Suite/ service 3
+            for (let i = 0; i < suitesDetails.length; i++){
+                let randomSuitesDetails3 = suitesDetails[Math.floor(Math.random() * suitesDetails.length)]; // Place this random selection variable outisde the for loop, unlike in the loop above, otherwise the randomly selected city weather might not match the city you want.
+
+                
+                const iconsrcSuites3 = `${randomSuitesDetails3.photo}`;
+                const descSuites3 = `Photo of ${randomSuitesDetails3.name}`;
+            // Suite/ service 4
+            // for (let i = 0; i < suitesDetails.length; i++){
+            //     let randomSuitesDetails4 = suitesDetails[Math.floor(Math.random() * suitesDetails.length)]; // Place this random selection variable outisde the for loop, unlike in the loop above, otherwise the randomly selected city weather might not match the city you want.
+
+                
+            //     const iconsrcSuites4 = `${randomSuitesDetails4.photo}`;
+            //     const descSuites4 = `Photo of ${randomSuitesDetails4.name}`;
+            // Appending suites and other rooms
+            document.querySelector('#random-suite1').setAttribute('src', iconsrcSuites1);
+            document.querySelector('#random-suite1').setAttribute('alt', descSuites1);
+            document.querySelector('#random-suite1-name').innerHTML = randomSuitesDetails1.name;
+
+            document.querySelector('#random-suite2').setAttribute('src', iconsrcSuites2);
+            document.querySelector('#random-suite2').setAttribute('alt', descSuites2);
+            document.querySelector('#random-suite2-name').innerHTML = randomSuitesDetails2.name;
+
+            document.querySelector('#random-suite3').setAttribute('src', iconsrcSuites3);
+            document.querySelector('#random-suite3').setAttribute('alt', descSuites3);
+            document.querySelector('#random-suite3-name').innerHTML = randomSuitesDetails3.name;
+
+            // document.querySelector('#random-suite4').setAttribute('src', iconsrcSuites4);
+            // document.querySelector('#random-suite4').setAttribute('alt', descSuites4);
+            // document.querySelector('#random-suite4-name').innerHTML = randomSuitesDetails4.name;
+            // document.querySelector('#random-address').innerHTML = randomSuitesDetails.address;
+            // document.querySelector('#random-telephone').innerHTML = randomSuitesDetails.telephone;
+            // document.querySelector('#random-history').innerHTML = randomSuitesDetails.history;
+            };
+            };
+            };
+            // };
+        });
+
+// LIKE/ DISLIKE TOGGLE
+
+function likeDislikeToggle(x) {
+    x.classList.toggle("fa-thumbs-down");
+  }
+
+  // COUNT LIKES
+function clickCounterLikes() {
+if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+    localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+    localStorage.clickcount = 1;
+    }
+    document.getElementById("result1").innerHTML = localStorage.clickcount;
+} else {
+    document.getElementById("result1").innerHTML = "Sorry, your browser does not support web storage...";
+}
+}
+
+function clickCounterDislikes() {
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+        localStorage.clickcount = Number(localStorage.clickcount)+1;
+        } else {
+        localStorage.clickcount = 1;
+        }
+        document.getElementById("result2").innerHTML = localStorage.clickcount;
+    } else {
+        document.getElementById("result2").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+    }
